d269ac9a343c76d352852d5966b96fe7c9867ba5
John M. Schanck <jschanck@uwaterloo.ca>
2020-09-02 10:53:52 -0400

    add checkout.sh

---
+++ checkout.sh
@@ -0,0 +1,38 @@
+V1=supercop-20200826
+V2=supercop-20200826-patched
+
+PYTHON=/usr/bin/python3
+
+WORKDIR=`dirname $0`
+WORKDIR=`cd ${WORKDIR} && pwd`
+PATCHES=${WORKDIR}/patches
+SCRIPTS=${WORKDIR}/scripts
+
+cd ${WORKDIR}
+
+if [ ! -e "${V1}" ]
+then
+  if [ ! -f ${V1}.tar.xz ]
+  then
+    wget https://bench.cr.yp.to/supercop/${V1}.tar.xz
+  fi
+  unxz < ${V1}.tar.xz | tar -xf -
+fi
+
+if [ -e "${V2}" ]
+then
+  read -p "${V2} directory already exists. Delete it? " yn
+  if [ "${yn:-n}" != "y" ]
+  then
+    exit -1
+  fi
+  rm -rf ${V2}
+fi
+
+cp -rp ${V1} ${V2}
+( cd ${V2}
+for X in ${PATCHES}/*
+do
+  patch -p1 < ${X}
+done
+)
