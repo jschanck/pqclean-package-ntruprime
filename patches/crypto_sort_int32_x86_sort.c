1a3ec09e2d02ea855af8a85753f99dec45110a3c
John M. Schanck <jschanck@uwaterloo.ca>
2020-09-02 13:25:43 -0400

    NTRU Prime: go directly to crypto_sort_smallindices

--- patches/crypto_sort_int32_x86_sort.c
+++ patches/crypto_sort_int32_x86_sort.c
@@ -1,6 +1,6 @@
 --- supercop-20200826/crypto_sort/int32/x86/sort.c
 +++ supercop-20200826-patched/crypto_sort/int32/x86/sort.c
-@@ -1,8 +1,18 @@
+@@ -1,11 +1,21 @@
 -#include "crypto_sort.h"
 -#include "crypto_int32.h"
 -#define int32 crypto_int32
@@ -22,8 +22,12 @@
 +} while(0)
  
  /* assume 2 <= n <= 0x40000000 */
- static void crypto_sort_smallindices(int32 *x,int32 n)
-@@ -87,23 +97,27 @@
+-static void crypto_sort_smallindices(int32 *x,int32 n)
++void crypto_sort(int32_t *x,long long n)
+ {
+   int32 top,p,q,r,i,j;
+ 
+@@ -25,78 +35,18 @@
      i = 0;
      j = 0;
      for (q = top;q > p;q >>= 1) {
@@ -37,14 +41,72 @@
 -        if (j == i + p) {
 -          i += 2 * p;
 -          break;
+-        }
+-      }
+-      while (i + p <= n - q) {
+-        for (j = i;j < i + p;++j) {
 +      if (j != i) {
 +        for (;;) {
 +          if (j == n - q) goto done;
-+          int32 a = x[j + p];
-+          for (r = q;r > p;r >>= 1) {
+           int32 a = x[j + p];
+           for (r = q;r > p;r >>= 1)
+-            int32_MINMAX(a,x[j+r]);
 +            int32_MINMAX(a,x[j + r]);
-+          }
-+          x[j + p] = a;
+           x[j + p] = a;
+-        }
+-        i += 2 * p;
+-      }
+-      /* now i + p > n - q */
+-      j = i;
+-      while (j < n - q) {
+-        int32 a = x[j + p];
+-        for (r = q;r > p;r >>= 1)
+-          int32_MINMAX(a,x[j+r]);
+-        x[j + p] = a;
+-        ++j;
+-      }
+-
+-      done: ;
+-    }
+-  }
+-}
+-
+-void crypto_sort(void *array,long long n)
+-{
+-  long long top,p,q,r,i,j;
+-  int32 *x = array;
+-
+-  if (n < 2) return;
+-  if (n < 0x40000000) {
+-    crypto_sort_smallindices(x,n);
+-    return;
+-  }
+-  top = 1;
+-  while (top < n - top) top += top;
+-
+-  for (p = top;p >= 1;p >>= 1) {
+-    i = 0;
+-    while (i + 2 * p <= n) {
+-      for (j = i;j < i + p;++j)
+-        int32_MINMAX(x[j],x[j+p]);
+-      i += 2 * p;
+-    }
+-    for (j = i;j < n - p;++j)
+-      int32_MINMAX(x[j],x[j+p]);
+-
+-    i = 0;
+-    j = 0;
+-    for (q = top;q > p;q >>= 1) {
+-      if (j != i) for (;;) {
+-        if (j == n - q) goto done;
+-        int32 a = x[j + p];
+-        for (r = q;r > p;r >>= 1)
+-          int32_MINMAX(a,x[j + r]);
+-        x[j + p] = a;
+-        ++j;
+-        if (j == i + p) {
+-          i += 2 * p;
+-          break;
 +          ++j;
 +          if (j == i + p) {
 +            i += 2 * p;
@@ -53,24 +115,4 @@
          }
        }
        while (i + p <= n - q) {
-         for (j = i;j < i + p;++j) {
-           int32 a = x[j + p];
--          for (r = q;r > p;r >>= 1)
-+          for (r = q;r > p;r >>= 1) {
-             int32_MINMAX(a,x[j+r]);
-+          }
-           x[j + p] = a;
-         }
-         i += 2 * p;
-@@ -112,8 +126,9 @@
-       j = i;
-       while (j < n - q) {
-         int32 a = x[j + p];
--        for (r = q;r > p;r >>= 1)
-+        for (r = q;r > p;r >>= 1) {
-           int32_MINMAX(a,x[j+r]);
-+        }
-         x[j + p] = a;
-         ++j;
-       }
 
